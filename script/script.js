53bcb65cebdedaeddd2a402ba6542641781c69d2
BubaiShil <bubaishil56@gmail.com>
2024-06-15 01:54:58 +0530

    added feature addtocart

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+
+
 const items = [
     {
         id: '001',
@@ -118,3 +120,60 @@ const items = [
         },
     }
 ];
+
+let bagitems = []
+
+onLoad()
+function onLoad() {
+    home()
+    numberOfItem()
+    let bagstr=localStorage.getItem('bagitem')
+    bagitems = bagstr ? JSON.parse(bagstr) : [];
+}
+
+function addtocart(itemid) {
+    bagitems.push(itemid)
+    numberOfItem()
+    localStorage.setItem('bagitem',JSON.stringify(bagitems))
+}
+
+
+function numberOfItem() {
+    let itemNo = document.querySelector(".bag-items")
+    if (bagitems.length > 0) {
+        itemNo.style.visibility = 'visible'
+        itemNo.innerText = bagitems.length
+    } else {
+        itemNo.style.visibility = 'hidden'
+    }
+}
+
+
+function home() {
+    const maincontainer = document.querySelector(".items-container")
+    let innerHTML = " "
+    items.forEach(item => {
+        innerHTML += `
+
+            <div class="item-container">
+                <img class="item-img" src="${item.image}" alt="loading...">
+                <div class="rating">${item.rating.stars} ⭐ ${item.rating.count}</div>
+                <div class="company">${item.company}</div>
+                <div class="items-name">${item.item_name}</div>
+                <div class="price">
+                    <span class="currentprice">${item.current_price}</span>
+                    <span class="originalprice">${item.original_price}</span>
+                    <span class="discountprice">${item.discount}</span>
+                </div>
+                <button class="addbag" onclick= "addtocart(${item.id})">Add to Bag</button>
+            </div>
+
+`
+    })
+
+    maincontainer.innerHTML = innerHTML;
+}
+
+
+
+
